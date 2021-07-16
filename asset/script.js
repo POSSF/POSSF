70b01cccb9107e0a7f664ea86964f42ce24718c2
Max Base <MaxBaseCode@gmail.com>
2021-07-17 02:59:41 +0430

    fix errors of script.js

--- asset/script.js
+++ asset/script.js
@@ -1,22 +1,15 @@
-let nav_icon = document.querySelector("header .small-nav-icon");
-let nav = document.querySelector("header nav");
-nav_icon.addEventListener("click", function() {
-	nav.classList.toggle("active");
-});
-
-let faq_items = document.querySelectorAll(".faq-item");
-if(faq_items) {
-	for(let faq of faq_items) {
-		console.log(faq);
-		faq.querySelector(".faq-box").addEventListener("click", function() {
-			console.log("faq item toggle!");
-			faq.classList.toggle("active");
-		});
-	}
+// functions
+function openModal(modal) {
+	if (modal == null) return
+	modal.classList.add('active')
+	overlay.classList.add('active')
 }
 
-let dialog_bg = document.querySelector("#dialog-subscribe-background");
-let dialog_main = document.querySelector("#dialog-subscribe");
+function closeModal(modal) {
+	if (modal == null) return
+	modal.classList.remove('active')
+	overlay.classList.remove('active')
+}
 
 function subscribe() {
 	console.log("Open subscribe dialog...");
@@ -36,39 +29,59 @@ function subscribe_close(dialog) {
 	dialog.classList.remove("active");
 	dialog_bg.classList.remove("active");
 }
+
+// variables
+const nav = document.querySelector("header nav");
+const nav_icon = document.querySelector("header .small-nav-icon");
+
+const faq_items = document.querySelectorAll(".faq-item");
+
+const dialog_bg = document.querySelector("#dialog-subscribe-background");
+const dialog_main = document.querySelector("#dialog-subscribe");
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
-openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-  })
-})
-
-overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
-  modals.forEach(modal => {
-    closeModal(modal)
-  })
-})
-
-closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
-  })
-})
-
-function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
+// events
+if(nav_icon) {
+	nav_icon.addEventListener("click", function() {
+		nav.classList.toggle("active");
+	});
 }
 
-function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
+if(faq_items) {
+	for(const faq of faq_items) {
+		faq.querySelector(".faq-box").addEventListener("click", function() {
+			console.log("faq item toggle!");
+			faq.classList.toggle("active");
+		});
+	}
+}
+
+
+if(openModalButtons) {
+	openModalButtons.forEach(button => {
+		button.addEventListener('click', () => {
+			const modal = document.querySelector(button.dataset.modalTarget)
+			openModal(modal)
+		})
+	})
+}
+
+if(overlay) {
+	overlay.addEventListener('click', () => {
+		const modals = document.querySelectorAll('.modal.active')
+			modals.forEach(modal => {
+			closeModal(modal)
+		})
+	})
+}
+
+if(closeModalButtons) {
+	closeModalButtons.forEach(button => {
+		button.addEventListener('click', () => {
+			const modal = button.closest('.modal')
+			closeModal(modal)
+		})
+	})
 }
